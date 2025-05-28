067441231904c3d4f19cf94021f2e7e558a76aa6
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-28 14:59:22 -0300

    Updated clock_divider.sv

--- HDL/clock_divider.sv
+++ HDL/clock_divider.sv
@@ -1,34 +1,41 @@
 module divider (
-  input wire clock1M,
-  input wire reset,
-  output logic clk_10KHz,
-  output logic clk_100KHz
+    input wire clock1M,
+    input wire reset,
+    output logic clk_10KHz,
+    output logic clk_100KHz
 );
 
-  reg [0:7] clk10_count;
-  reg [0:7] clk100_count;
+  reg [7:0] clk10_count;
+  reg [7:0] clk100_count;
 
+  // clk_100KHz
   always_ff @(posedge clock1M or posedge reset) begin
-        if (reset) begin
-            clk_10KHz <= 0;
-            clk_100KHz <= 0;
-            clk10_count <= 0;
-            clk100_count <= 0;
-        end else begin
-          if(clk100_count == 4) begin 
-            clk_100KHz <= ~clk_100KHz;
-            clk100_count <= 0;
-          end else begin
-            clk100_count <= clk100_count + 1;
-          end 
-          if(clk10_count == 49) begin 
-            clk_10KHz <= ~clk_10KHz;
-            clk10_count <= 0;
-          end else begin
-            clk10_count <= clk10_count + 1;
-          end
-        end
+    if (reset) begin
+      clk_100KHz <= 0;
+      clk100_count <= 0;
+    end else begin
+      if (clk100_count == 4) begin // 1MHz / (4+1)/2 = 100KHz
+        clk_100KHz <= ~clk_100KHz;
+        clk100_count <= 0;
+      end else begin
+        clk100_count <= clk100_count + 1;
+      end
+    end
   end
-endmodule
 
-    
+  // clk_10KHz
+  always_ff @(posedge clock1M or posedge reset) begin
+    if (reset) begin
+      clk_10KHz <= 0;
+      clk10_count <= 0;
+    end else begin
+      if (clk10_count == 49) begin // 1MHz / (49+1)/2 = 10KHz
+        clk_10KHz <= ~clk_10KHz;
+        clk10_count <= 0;
+      end else begin
+        clk10_count <= clk10_count + 1;
+      end
+    end
+  end
+
+endmodule
