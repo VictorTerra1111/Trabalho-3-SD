cfbfe7fdffeed4003fe203f4dc9e3e058fe73d68
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-28 09:35:36 -0300

    Updated fila.sv

--- HDL/fila.sv
+++ HDL/fila.sv
@@ -1,43 +1,45 @@
 module fila(
-    input [7:0] data_in,
-    input enqueue_in,
-    input dequeue_in
-    
-    input reset,
-    input clk_10KHz,
-    
-    output data_out,
-    output len_out,
+    input logic [7:0] data_in,
+    input logic enqueue_in,
+    input logic dequeue_in,
+    input logic reset,
+    input logic clk_10KHz,
+
+    output logic [7:0] data_out,
+    output logic [2:0] len_out // até 8 elementos
 );
 
-    logic [7:0][7:0] vector;
-    logic tam_vet;
-    
-    assign len_out = tam_vet;
+    logic [7:0] vector [7:0]; // 8 posições de 8 bits
+    logic [2:0] tam_vet;
 
     integer i;
 
-    always_ff @(posedge clk_10KHz) begin
+    always_ff @(posedge clk_10KHz or posedge reset) begin
         if (reset) begin
-            data_out <= 0;
+            data_out <= 8'b0;
             len_out <= 0;
             tam_vet <= 0;
 
-            for(i; i < 8; i <= i + 1) begin
+            for (i = 0; i < 8; i = i + 1) begin
                 vector[i] <= 8'b0;
             end
         end else begin
-                if(enqueue_in) begin
-                    vector[tam_vet] <= data_in;
-                    tam_vet <= tam_vet + 1;
-                end 
-                else if(dequeue_in) begin
-                    data_out <= vector[0];
-                    vector[0:6] <= vector [1:7];
-                    vector[7] <= 8'b0;
-                    tam_vet <= tam_vet - 1;
-                end 
-            end
-        end 
-    end 
+            if (enqueue_in && tam_vet < 8) begin
+                vector[tam_vet] <= data_in;
+                tam_vet <= tam_vet + 1;
+            end 
+            else if (dequeue_in && tam_vet > 0) begin
+                data_out <= vector[0];
 
+                // Shift manual
+                for (i = 0; i < 7; i = i + 1) begin
+                    vector[i] <= vector[i+1];
+                end
+                vector[7] <= 8'b0;
+                tam_vet <= tam_vet - 1;
+            end
+
+            len_out <= tam_vet;
+        end
+    end
+endmodule
