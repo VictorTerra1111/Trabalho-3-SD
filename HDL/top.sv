49e598ac4c70efd66e4d93a4bf7bb4f0fff8fa3d
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-28 14:22:17 -0300

    Update top.sv

--- HDL/top.sv
+++ HDL/top.sv
@@ -1,27 +1,17 @@
-/*
-Criação de um módulo top. A cada 8 bits recebidos pelo
-deserializador, uma palavra de 8 bits deverá ser colocada na pilha. 
-O módulo top deverá receber
-um sinal de clock de 1MHz. Dois processos internos deverão utilizar 
-o sinal de 1MHz para
-gerar dois sinais de clock diferentes: um de 100KHz e outro de 10KHz, alimentando os
-módulos anteriores.
-*/
-
 module top(
     input logic clock1M,
     input logic reset,
 
-    input logic data_in, // vai para deserializador
-    input logic write_in, // vai para deserializador
-    input logic dequeue_in, // vai para fila
+    input logic data_in, 
+    input logic write_in, 
+    input logic dequeue_in,
 
-    output logic data_out, // vem de fila
-    output logic status_out // vem do deserializador
+    output logic data_out,
+    output logic status_out 
 );
 
     logic enqueue_in, ack_in, clk_10KHz, clk_100KHz;
-    logic data_ready_des; // sai de des e vai para fila no enqueue_in
+    logic data_ready_des; 
     logic [7:0] data_out_des;
     assign ack_in = (len_out < 8);
 
@@ -29,23 +19,23 @@ deserializador des(
     .reset(reset),
     .clk_100KHz(clk_100KHz),
     
-    .data_in(data_in),                  // input do modulo
-    .write_in(write_in),                // input do modulo
-    .status_out(status_out),            // output do modulo
-    .ack_in(ack_in),                    // vem de fila
-    .data_out(data_out_des),             // vem do des vai para fila (data in)
-    .data_ready(data_ready_des)         // sai do des vai para faila
+    .data_in(data_in),               
+    .write_in(write_in),           
+    .status_out(status_out),        
+    .ack_in(ack_in),                
+    .data_out(data_out_des),            
+    .data_ready(data_ready_des)        
 );  
 
 fila fil(
     .reset(reset),
     .clk_10KHz(clk_10KHz),
 
-    .data_in(data_out_des),             // vem de des
-    .len_out(ack_in),                  // vem de des
-    .enqueue_in(data_ready_des),        // vem do des
-    .data_out(data_out),                // vai para top
-    .dequeue_in(dequeue_in)             // vem do top
+    .data_in(data_out_des),          
+    .len_out(ack_in),                
+    .enqueue_in(data_ready_des),       
+    .data_out(data_out),                
+    .dequeue_in(dequeue_in)           
 );
 
 divider divisor(
