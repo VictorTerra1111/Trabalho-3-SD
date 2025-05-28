ce2cc9340701422319a160e41233c3e152c16ec5
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-28 17:42:41 +0000

    changed tb divisor

--- SIM/tb_do_divisor/tb_divisor.sv
+++ SIM/tb_do_divisor/tb_divisor.sv
@@ -2,38 +2,30 @@
 
 module tb_clock_divider();
 
-    // Entradas
     logic clock1M;
     logic reset;
 
-    // Saídas
     logic clk_10KHz;
     logic clk_100KHz;
 
-    // Instanciação do módulo a ser testado
-    divider uut (
+    divider dut (
         .clock1M(clock1M),
         .reset(reset),
         .clk_10KHz(clk_10KHz),
         .clk_100KHz(clk_100KHz)
     );
 
-    // Gera clock de 1MHz (período de 1us)
     always #0.5 clock1M = ~clock1M;
 
     initial begin
-        // Inicialização
         clock1M = 0;
         reset = 1;
         
-        // Aplica reset
         #2;
         reset = 0;
 
-        // Roda por alguns ciclos para observar as saídas
         repeat (200) @(posedge clock1M);
 
-        // Aplica reset novamente para testar comportamento
         reset = 1;
         #2;
         reset = 0;
@@ -41,12 +33,6 @@ module tb_clock_divider();
         repeat (100) @(posedge clock1M);
 
         $finish;
-    end
-
-    // Monitoramento das saídas
-    initial begin
-        $display("Tempo(ns)\treset\tclk_10KHz\tclk_100KHz");
-        $monitor("%0t\t%b\t%b\t\t%b", $time, reset, clk_10KHz, clk_100KHz);
     end
 
 endmodule
