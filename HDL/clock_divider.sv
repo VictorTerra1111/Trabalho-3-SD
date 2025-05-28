6bbe473991fa67d0e5a73131bd04064ef6a077a2
Raul Costa Soares <114699737+RaulCostaSoares@users.noreply.github.com>
2025-05-28 09:06:26 -0300

    Create clock_divider.sv
    criando o modulo do divisor de clock

---
+++ HDL/clock_divider.sv
@@ -0,0 +1,34 @@
+module divider (
+  input wire clk_in,
+  input wire reset,
+  output logic clk_10KHz,
+  output logic clk_100KHz
+)
+
+  reg [0:7] clk10_count;
+  reg [0:7] clk100_count;
+
+  always_ff @(posedge clk_in or posedge reset) begin
+        if (reset) begin
+            clk_10KHz <= 0;
+            clk_100KHz <= 0;
+            clk10_count <= 0;
+            clk100_count <= 0;
+        end else begin
+          if(clk100_count == 4) begin 
+            clk_100KHz <= ~clk_100KHz;
+            clk100_count <= 0;
+          end else begin
+            clk100_count <= clk100_count + 1;
+          end 
+          if(clk10_count == 49) begin 
+            clk_10KHz <= ~clk_10KHz;
+            clk10_count <= 0;
+          end else begin
+            clk10_count <= clk10_count + 1;
+          end
+        end
+  end
+endmodule
+
+    
