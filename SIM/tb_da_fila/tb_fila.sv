7d4aaf8bad9eb92d5481538d19de514b329d4223
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-28 17:44:11 +0000

    changed tb fila

--- SIM/tb_da_fila/tb_fila.sv
+++ SIM/tb_da_fila/tb_fila.sv
@@ -11,7 +11,6 @@ module tb_fila;
     wire [7:0] data_out;
     wire [2:0] len_out;
 
-    // Instancia o DUT (Device Under Test)
     fila dut (
         .data_in(data_in),
         .enqueue_in(enqueue_in),
@@ -22,7 +21,6 @@ module tb_fila;
         .len_out(len_out)
     );
 
-    // Geração do clock de 10KHz (período = 100us)
     initial clk_10KHz = 0;
     always #50 clk_10KHz = ~clk_10KHz;
 
@@ -44,7 +42,6 @@ module tb_fila;
     endtask
 
     initial begin
-        // Inicialização
         data_in = 0;
         enqueue_in = 0;
         dequeue_in = 0;
@@ -52,25 +49,14 @@ module tb_fila;
         @(posedge clk_10KHz);
         reset = 0;
 
-        // Enfileira 3 dados
         enqueue(8'hA1);
         enqueue(8'hB2);
         enqueue(8'hC3);
 
-        // Espera 1 ciclo
         @(posedge clk_10KHz);
 
-        $display("Len after enqueue 3: %0d", len_out); // Esperado: 3
-
-        // Desenfileira 2 vezes
         dequeue();
-        $display("Data out 1: %h (esperado: A1)", data_out);
         dequeue();
-        $display("Data out 2: %h (esperado: B2)", data_out);
-
-        $display("Len after dequeue 2: %0d", len_out); // Esperado: 1
-
-        // Finaliza simulação
         #200;
         $finish;
     end
