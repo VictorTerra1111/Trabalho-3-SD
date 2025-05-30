69a2545b80559150865491d2d76158de9733dc8a
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-30 20:21:09 -0300

    Update fila.sv

--- HDL/fila.sv
+++ HDL/fila.sv
@@ -11,30 +11,35 @@ module fila(
 
     logic [7:0] vector [7:0]; 
     logic [2:0] tam_vet;
+    logic dequeue_selecionado;
 
     integer i;
 
-    always @(posedge clk_10KHz or posedge reset) begin
+    always_ff @(posedge clk_10KHz or posedge reset) begin
         if (reset) begin
             data_out <= 8'b0;
             len_out <= 0;
             tam_vet <= 0;
-            for (i = 0; i < 8; i = i + 1) begin
+            dequeue_selecionado <= 0;
+            for (i = 0; i < 8; i = i + 1)
                 vector[i] <= 8'b0;
-            end
         end else begin
             if (enqueue_in && tam_vet < 8) begin
                 vector[tam_vet] <= data_in;
                 tam_vet <= tam_vet + 1;
-            end else if (dequeue_in && tam_vet > 0) begin
-                data_out <= vector[0];
+            end
 
-                for (i = 0; i < 7; i = i + 1) begin
+            if (dequeue_selecionado) begin
+                data_out <= vector[0];
+                for (i = 0; i < 7; i = i + 1)
                     vector[i] <= vector[i+1];
-                end
                 vector[7] <= 8'b0;
                 tam_vet <= tam_vet - 1;
+                dequeue_selecionado <= 0;
+            end else if (dequeue_in && tam_vet > 0) begin
+                dequeue_selecionado <= 1;
             end
+
             len_out <= tam_vet;
         end
     end
