fd3004b93e0b509a3f742895cc9a0e985c21905a
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-30 09:25:59 -0300

    Update verifica_len_out.sv

--- HDL/verifica_len_out.sv
+++ HDL/verifica_len_out.sv
@@ -2,8 +2,6 @@ module verifica_len_out(
   input logic [2:0] len_out,
   output logic ack_in 
 );
-
-  if(len_out == 8) begin
-    ack_in = 1;
-  end else ack_in = 0;
+  assign ack_in = (len_out < 8);
+  
 endmodule
