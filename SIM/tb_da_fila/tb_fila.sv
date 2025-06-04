51159e91e8b84b10ad5375f6c3a06cf1bf7c78e4
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-04 13:21:22 -0300

    Update tb_fila.sv

--- SIM/tb_da_fila/tb_fila.sv
+++ SIM/tb_da_fila/tb_fila.sv
@@ -2,75 +2,112 @@
 
 module tb_fila;
 
-    logic [7:0] data_in;
-    logic enqueue_in;
-    logic dequeue_in;
-    logic reset;
-    logic clk_10KHz;
-    logic [7:0] data_out;
-    logic [7:0] len_out;
+logic [7:0] data_in;
 
-    fila dut (
-        .data_in(data_in),
-        .enqueue_in(enqueue_in),
-        .dequeue_in(dequeue_in),
-        .reset(reset),
-        .clk_10KHz(clk_10KHz),
-        .data_out(data_out),
-        .len_out(len_out)
-    );
+logic enqueue_in;
 
-    initial clk_10KHz = 0;
-    always #50 clk_10KHz = ~clk_10KHz;
+logic dequeue_in;
 
-    initial begin
-        data_in = 8'd0;
-        enqueue_in = 0;
-        dequeue_in = 0;
-        reset = 1;
-        #100;
-        reset = 0;
-        
-        enqueue_in = 1;  #100;
-        data_in = 8'h11; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h22; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h33; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h44; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h55; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h66; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h77; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h88; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        data_in = 8'h99; #100; 
-        enqueue_in = 0;  #100;
-        enqueue_in = 1;  #100;
-        
-        #100;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        dequeue_in = 1; #100; dequeue_in = 0;
-        #100;
-        $finish;
-    end
+logic reset;
+
+logic clk_10KHz;
+
+logic [7:0] data_out;
+
+logic [7:0] len_out;
+
+
+
+fila dut (
+
+    .data_in(data_in),
+
+    .enqueue_in(enqueue_in),
+
+    .dequeue_in(dequeue_in),
+
+    .reset(reset),
+
+    .clk_10KHz(clk_10KHz),
+
+    .data_out(data_out),
+
+    .len_out(len_out)
+
+);
+
+
+
+initial clk_10KHz = 0;
+
+always #50 clk_10KHz = ~clk_10KHz;
+
+
+
+initial begin
+
+    data_in = 8'd0;
+
+    enqueue_in = 0;
+
+    dequeue_in = 0;
+
+    reset = 1;
+
+    #100;
+
+    reset = 0;
+
+
+
+    data_in = 8'h11; enqueue_in = 1; #50; 
+
+    data_in = 8'h22; #50;
+
+    data_in = 8'h33; #50; 
+
+    data_in = 8'h44; #50; 
+
+    data_in = 8'h55; #50; 
+
+    data_in = 8'h66; #50; 
+
+    data_in = 8'h77; #50; 
+
+    data_in = 8'h88; #50;
+
+    data_in = 8'h99; #50;
+
+    enqueue_in = 0;
+
+    #100;
+
+    #50;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+    dequeue_in = 1; #500; dequeue_in = 0;
+
+
+
+    #100;
+
+    $finish;
+
+end
+
 endmodule
