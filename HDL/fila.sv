bdd29516a5ca63a9186a14c6c8307310b0df6b71
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-04 09:27:48 -0300

    Update fila.sv

--- HDL/fila.sv
+++ HDL/fila.sv
@@ -9,39 +9,70 @@ module fila(
     output logic [7:0] len_out
 );
 
+    typedef enum logic [1:0] {
+        wait_s,
+        dequeue,
+        at_fila,
+        enviado
+    } state_t;
+
+    state_t state, next_state;
+
     logic [7:0] vector [7:0];
     logic [7:0] tam_vet;
-    logic dequeue_selecionado;
-
     integer i;
 
     always @(posedge clk_10KHz or posedge reset) begin
         if (reset) begin
-            data_out <= 8'b0;
-            len_out <= 8'b0;
-            tam_vet <= 8'b0;
-            dequeue_selecionado <= 1'b0;
+            state <= wait;
+            tam_vet <= 8'd0;
+            data_out <= 8'd0;
+            len_out <= 8'd0;
             for (i = 0; i < 8; i = i + 1)
-                vector[i] <= 8'b0;
+                vector[i] <= 8'd0;
         end else begin
-            if (enqueue_in && tam_vet < 8) begin
-                vector[tam_vet] <= data_in;
-                tam_vet <= tam_vet + 1; // TAMANHO ESTA SENDO ATUALIZADO MAS NAO APARECE O NOVO NO LEN_OUT
-                len_out <= tam_vet + 1;
-            end
+            state <= next_state;
 
-            if (dequeue_in && tam_vet > 0 && !dequeue_selecionado) begin
-                dequeue_selecionado <= 1'b1;
-                data_out <= vector[0];
-            end else if (dequeue_selecionado) begin
-                for (i = 0; i < 7; i = i + 1)
-                    vector[i] <= vector[i+1];
-                vector[7] <= 8'b0;
-                tam_vet <= tam_vet - 1;
-                len_out <= tam_vet - 1;
-                dequeue_selecionado <= 1'b0;
-            end
-           // len_out <= tam_vet; // ALTERAR
+            case (state)
+                wait_s: begin
+                    if (enqueue_in && tam_vet < 8) begin
+                        vector[tam_vet] <= data_in;
+                        tam_vet <= tam_vet + 1;
+                    end
+                end
+
+                dequeue: begin
+                    data_out <= vector[0];
+                end
+
+                at_fila: begin
+                    for (i = 0; i < 7; i = i + 1)
+                        vector[i] <= vector[i+1];
+                    vector[7] <= 8'd0;
+                end
+
+                enviado: begin
+                    tam_vet <= tam_vet - 1;
+                end
+            endcase
+
+            len_out <= tam_vet;
         end
     end
+
+    always_comb begin
+        case (state)
+            wait: begin
+                if (dequeue_in && tam_vet > 0)
+                    next_state = dequeue;
+                else
+                    next_state = wait;
+            end
+            dequeue: next_state = at_fila;
+            at_fila: next_state = enviado;
+            enviado: next_state = wait;
+            default: next_state = wait;
+        endcase
+    end
+
 endmodule
