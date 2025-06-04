757d1a6543497443de3bca473fffec75336d95f1
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-04 10:18:15 -0300

    Update fila.sv

--- HDL/fila.sv
+++ HDL/fila.sv
@@ -19,7 +19,6 @@ always @(posedge clk_10KHz or posedge reset) begin
         data_out <= 8'b0;  
         len_out <= 8'b0;  
         len_out <= 8'b0;  
-        dequeue_selecionado <= 1'b0;  
         vector[i] <= 8'b0;  
     end else begin  
         if (enqueue_in && len_out < 8) begin  
@@ -27,7 +26,7 @@ always @(posedge clk_10KHz or posedge reset) begin
             len_out <= len_out + 1;
         end  
 
-        if (dequeue_in && len_out > 0 && !dequeue_selecionado) begin  
+        if (dequeue_in && len_out > 0) begin  
             data_out <= vector[0];  
             for (i = 0; i < 7; i = i + 1)  
                 vector[i] <= vector[i+1];  
