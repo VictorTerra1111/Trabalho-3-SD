155a5bcd4516d302ed9e587f379e39952b3884b9
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-04 08:58:18 -0300

    Update fila.sv

--- HDL/fila.sv
+++ HDL/fila.sv
@@ -10,7 +10,7 @@ module fila(
 );
 
     logic [7:0] vector [7:0];
-    logic [3:0] tam_vet;
+    logic [7:0] tam_vet;
     logic dequeue_selecionado;
 
     integer i;
@@ -19,7 +19,7 @@ module fila(
         if (reset) begin
             data_out <= 8'b0;
             len_out <= 8'b0;
-            tam_vet <= 4'b0;
+            tam_vet <= 8'b0;
             dequeue_selecionado <= 1'b0;
             for (i = 0; i < 8; i = i + 1)
                 vector[i] <= 8'b0;
